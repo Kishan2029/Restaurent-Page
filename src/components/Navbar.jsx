e2a3a1308a8516a94866b3f9b965df77c62e9a3f
kishan-2029 <kishan.prajapati@searce.com>
2023-06-25 16:26:58 +0530

    image code commented as it was giving error in deployment

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Burger from "../assets/vector.png";
-import Logo from "../assets/logo.svg";
+// import Logo from "../assets/logo.svg";
 
 function Navbar() {
   return (
