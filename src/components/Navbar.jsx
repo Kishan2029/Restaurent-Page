64de57d358c3334c92d6ff0cd70f0abde6a5a491
kishan-2029 <kishan.prajapati@searce.com>
2023-06-25 16:25:44 +0530

    image code commented as it was giving error in deployment

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ function Navbar() {
   return (
     <div className="flex justify-between mt-0 mx-12 ">
       {/* <div className="font-bold text-xl">XYZ Company</div> */}
-      <img className="h-24 w-40" src={Logo} />
+      {/* <img className="h-24 w-40" src={Logo} /> */}
       <img className="h-6 w-7 self-center" src={Burger} />
     </div>
   );
